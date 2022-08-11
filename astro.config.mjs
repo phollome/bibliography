df44aa9e7d87e63f92e96d4511854e40a57f63ef
Peter Holló <hello@songsforthe.dev>
2022-08-11 22:07:42 +0200

    Set up netlify identity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  projectRoot: ".",
+  astroRoot: "./src",
+  dist: "./dist",
+  public: "./public",
+  extensions: {
+    ".jsx": "react",
+  },
+  buildOptions: {
+    site: "https://melodious-gingersnap-70781e.netlify.app",
+    sitemap: true,
+  },
+  devOptions: {
+    port: 3000,
+    tailwindConfig: undefined,
+  },
+});
